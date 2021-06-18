c114e3281003199e34b7c6e12b22aa86f7fe6cdc
Dorukhan GÜLOZ <dguloz@havelsan.com.tr>
2021-06-18 19:13:36 +0300

    add server boilerplate code

---
+++ server/src/main/java/hvl/applogic/ApplogicApplication.java
@@ -0,0 +1,13 @@
+package hvl.applogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplogicApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/hvl/applogic/ApplogicApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.applogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
